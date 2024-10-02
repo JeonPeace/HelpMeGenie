4ff9dcb0edeee0a069fdb5f494d831f511c8c71d
DESKTOP-8E0GNTO\jeonpeace <jeonpeace0522@naver.com>
2024-10-02 21:19:10 +0900

    [전진우] - 초기 설정

---
+++ src/main/java/com/jeonpeace/helpmegenie/HelpMeGenieApplication.java
@@ -0,0 +1,13 @@
+package com.jeonpeace.helpmegenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpMeGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpMeGenieApplication.class, args);
+	}
+
+}
