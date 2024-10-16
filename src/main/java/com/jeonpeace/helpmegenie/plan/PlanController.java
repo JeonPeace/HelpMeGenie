a7e84a1b68e82f40cc83b5b7b9d7b33075e2440c
DESKTOP-8E0GNTO\jeonpeace <jeonpeace0522@naver.com>
2024-10-16 21:46:59 +0900

    [전진우-megait202] #11

--- src/main/java/com/jeonpeace/helpmegenie/plan/PlanController.java
+++ src/main/java/com/jeonpeace/helpmegenie/plan/PlanController.java
@@ -1,8 +1,15 @@
 package com.jeonpeace.helpmegenie.plan;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PlanController {
 
+	@GetMapping("/test-view")
+	public String calendarTest() {
+		
+		return "/test";
+	}
+	
 }
