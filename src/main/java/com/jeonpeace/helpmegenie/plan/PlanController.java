1ffbed844bd3779c239acb46156bc977f745a262
DESKTOP-8E0GNTO\jeonpeace <jeonpeace0522@naver.com>
2024-10-17 21:48:42 +0900

    [전진우-megait202] - fullcalendar 연구 진행중 #11

--- src/main/java/com/jeonpeace/helpmegenie/plan/PlanController.java
+++ src/main/java/com/jeonpeace/helpmegenie/plan/PlanController.java
@@ -2,14 +2,28 @@ package com.jeonpeace.helpmegenie.plan;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/plan")
 public class PlanController {
 
 	@GetMapping("/test-view")
 	public String calendarTest() {
 		
 		return "/test";
+	}
+	
+	@GetMapping("/progress-view")
+	public String progressView() {
+		
+		return "/plan/progress";
+	}
+	
+	@GetMapping("/new-plan-view")
+	public String createPlan() {
+		
+		return "/plan/create";
 	}
 	
 }
